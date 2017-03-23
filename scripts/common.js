2647ebb9c8532690b0116d268197f490656140f6
lius55 <tony56recruit@gmail.com>
2017-03-23 20:46:18 +0900

    add bill htmls

--- scripts/common.js
+++ scripts/common.js
@@ -1,12 +1,21 @@
 window.onload = function() {
 
 	// 三角形付きlabelの表示切り替え制御
-	$(".label-switch").on("click", function(){
+	$(".well-switch").on("click", function(){
 		// 表示切り替え対象要素id取得
-		var target_id = "#" + $(this).prop("for");
+		var target_id = "#" + $(this).attr("for");
 		// 対象要素の表示切り替え(表示、非表示)
 		$(target_id).toggleClass('panel-clicked');
 		// 三角形の表示切り替え(三角形の角度変更)
-		$(this).parent().toggleClass('clicked');
+		$(this).toggleClass('clicked');
 	});
+
+	// datepicker設定
+    $('.date').datepicker({
+    	format: 'yyyy/mm',// 日付フォマット
+    	minViewMode: '1', // デフォルトを月選択に設定
+    	language: 'ja'    // カレンダー日本語化のため
+    });
+    // デフォルトで当月表示する
+    $('.date').datepicker('setDate', new Date());
 }
